a9afef46487239248ee42df0083518518a782a65
Chien-hsueh <chienhsueh.h@gmail.com>
2021-04-19 01:38:25 -0600

    Code formatting/styling

--- src/App.js
+++ src/App.js
@@ -1,51 +1,63 @@
-import './App.css';
+import "./App.css";
 
-import {calculateClimb} from './util'
-import Footer from './components/Footer'
+import { calculateClimb } from "./util";
+import Footer from "./components/Footer";
 
-import {Button, TextField, Typography} from '@material-ui/core';
+import { Button, TextField, Typography } from "@material-ui/core";
 
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
 
 const App = () => {
-  const [numWays, setNumWays] = useState(0)
-  const [userInput, setUserInput] = useState(0)
-  const [buttonClicked, setButtonClicked] = useState(false)
+  const [numWays, setNumWays] = useState(0);
+  const [userInput, setUserInput] = useState(0);
+  const [buttonClicked, setButtonClicked] = useState(false);
 
-
-  const handleChange= (e) => {
-    setNumWays(calculateClimb(e.target.value))
-    setUserInput(e.target.value)
-  }
+  const handleChange = (e) => {
+    setNumWays(calculateClimb(e.target.value));
+    setUserInput(e.target.value);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
+        <div className="intro">
+          <Typography variant="h5">
+            You're a rebel scout on the ice planet, Hoth. While killing time
+            waiting for the Empire to find your base, you ride your trusty
+            Tauntaun around the plant. You wander upon a very tall ice staircase
+            leading up a mountainside that takes n steps to reach. Having plenty
+            of time on your hands, you decide you want to figure out how many
+            ways are there to reach the top. The catch is that for each step,
+            your Tauntauan can only climb 1 or 2 steps at a given time.
+          </Typography>
 
-      <div className="intro"> 
-      <Typography variant="h5">You're a rebel scout on the ice planet, Hoth. While killing time waiting for the Empire to find your base, you ride your trusty Tauntaun around the plant. You wander upon a very tall ice staircase leading up a mountainside that takes n steps to reach. Having plenty of time on your hands, you decide you want to figure out
-how many ways are there to reach the top. The catch is that for each step, your Tauntauan can only climb 1 or 2 steps at a given time.</Typography>
-      
-        <Button variant="contained" color="primary" onClick={() => setButtonClicked(true)}>▽ Start ▽</Button>
-      </div>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => setButtonClicked(true)}
+          >
+            ▽ Start ▽
+          </Button>
+        </div>
 
-      {buttonClicked && <TextField
-          id="step-input"
-          label="Enter number of steps"
-          type="number"
-          variant="outlined"
-          onChange={(e) => {handleChange(e)} }
-        />}
-      
-      <div className="footr">
-      <Footer userInput={userInput} numWays={numWays}/>
-      </div>
+        {buttonClicked && (
+          <TextField
+            id="step-input"
+            label="Enter number of steps"
+            type="number"
+            variant="outlined"
+            onChange={(e) => {
+              handleChange(e);
+            }}
+          />
+        )}
 
+        <div className="footr">
+          <Footer userInput={userInput} numWays={numWays} />
+        </div>
       </header>
-           
     </div>
   );
-}
+};
 
 export default App;
--- src/util.js
+++ src/util.js
@@ -1,22 +1,21 @@
 export const calculateClimb = (userInput) => {
-    
-    userInput = Number(userInput)
-    
-    if (userInput === '') return 0;
+  userInput = Number(userInput);
 
-    if (userInput === 0) {return 0;}
-    if (userInput === 1) {return 1;}
-    if (userInput === 2) {return 2;}
+  if (userInput === "") return 0;
 
-    let minusTwo = 1
-    let minusOne = 2
-    let current
+  if (userInput === 0) return 0;
+  if (userInput === 1) return 1;
+  if (userInput === 2) return 2;
 
-    for (let i= 3; i <= userInput; i++){
-    current = minusTwo + minusOne
-    minusTwo = minusOne
-    minusOne = current
-    }
+  let minusTwo = 1;
+  let minusOne = 2;
+  let current;
 
-    return current
+  for (let i = 3; i <= userInput; i++) {
+    current = minusTwo + minusOne;
+    minusTwo = minusOne;
+    minusOne = current;
+  }
+
+  return current;
 };
